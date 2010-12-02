fb1e94741827aca01285d7f91a884e3211cc64a9
Eddie Lovett <eddie@lovettcreations.org>
2010-12-02 10:58:15 -0500

    syncing for new site part2

---
+++ events/eventsNav.php
@@ -0,0 +1,11 @@
+<h2>Events & Career Fairs</h2>
+		<ul>
+			<li><a href="epackevents.php">ePACK</a></li>
+		  <li><a href="fairs.php">Career Fairs</a></li>
+			<li><a href="campusinterviews.php">On-Campus Interviews</a></li>
+			<li><a href="infosessions.php">Information Sessions</a></li>
+			<li><a href="orgs.php">Student Organization</a></li>
+			<li><a href="presentations.php">UCC Presentations</a></li>
+			<hr />
+			<li><a href="index.php" class="extraNav">Events & Career Fairs</a></li>
+		</ul>
