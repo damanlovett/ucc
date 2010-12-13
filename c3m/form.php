25b51ceb357973eae2df176ce34bef1bdbe587fd
Eddie Lovett <eddie@lovettcreations.org>
2010-12-13 10:10:39 -0500

    update -> refomated C3M software pages to match the current new website

--- c3m/form.php
+++ c3m/form.php
@@ -1,111 +1,66 @@
+<!DOCTYPE HTML>
 
-<?php require_once('includefiles/myfiles.php'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/second.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Career Fair Software Form </title>
-<!-- InstanceEndEditable -->
-<link href="../stylesheets/mainsheet.css" rel="stylesheet" type="text/css" />
-<!-- InstanceBeginEditable name="head" -->
-<style type="text/css">
-<!--
-.smallformbox {
-	font-size: 85%;
-}
--->
-</style><!-- InstanceEndEditable -->
+<meta charset="UTF-8">
+<title>University Career Center</title>
+<link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/base/base-min.css">
+<link rel="stylesheet" href="../css/uccdefaultNAV.css" type="text/css">
+<script src="../jquery/js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="../jquery/js/jquery-ui-1.8.2.custom.min.js" type="text/javascript"></script>
+<link href="../jquery/css/ucc/jquery-ui-1.8.2.custom.css" rel="stylesheet" type="text/css">
+<?php require_once('../includefiles/init.php'); ?>
+<?php require_once('includefiles/myfiles.php'); ?>
+
 </head>
 
-<body>
-<!-- DO NOT MOVE! The following AllWebMenus code must always be placed right AFTER the BODY tag-->
-<!-- ******** BEGIN ALLWEBMENUS CODE FOR maintopnav ******** -->
-<span id='xawmMenuPathImg-maintopnav' style='position:absolute;top:-50px'><img name='awmMenuPathImg-maintopnav' id='awmMenuPathImg-maintopnav' src='../menus/awmmenupath.gif' alt='' /></span>
-<script type='text/javascript'>var MenuLinkedBy='AllWebMenus [2]', awmBN='DW'; awmAltUrl='';</script>
-<script charset='UTF-8' src='../menus/maintopnav.js' language='JavaScript1.2' type='text/javascript'></script>
-<script type='text/javascript'>awmBuildMenu();</script>
-<!-- ******** END ALLWEBMENUS CODE FOR maintopnav ******** -->
-<div class="skipcontent">Skip to <a href="#content">content</a> or <a href="#links">page links</a> or <a href="#site">Site links</a></div>
-<table width="760" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" class="maintable">
-  <tr>
-    <td colspan="2"><img src="../images/header.jpg" alt="" width="760" height="55" border="0" usemap="#Map" /></td>
-  </tr>
-  <!-- InstanceBeginEditable name="PageHeader" -->
-  <tr>
-    <td align="left" valign="top" bgcolor="#000000" id="topquicklinks"><?php require_once('../menus/EMPquick.php'); ?></td>
-    <td width="581"><img src="../images/bannerstudent.jpg" alt="" width="581" height="136" /></td>
-  </tr>
-  <!-- InstanceEndEditable -->
-  <tr>
-    <td colspan="2" bgcolor="#999999"></td>
-  </tr>
-  <tr>
-    <td colspan="2"><span id='awmAnchor-maintopnav'>&nbsp;</span></td>
-  </tr>
-  <tr>
-    <td colspan="2"><table width="760" border="0" align="center" cellpadding="0" cellspacing="0" class="contenttable">
-      <tr><a name="links" id="links"></a>
-        <td width="195" align="left" valign="top" id="leftnav2"><!-- InstanceBeginEditable name="leftnav" -->
-          <?php require_once('../menus/STUjobsinterns.php'); ?>
-          <img src="../images/pageimages/resume.jpg" alt="" width="195" height="165" />
-          <p></p>
-        <!-- InstanceEndEditable --></td>
-        <td align="left" valign="top"><!-- InstanceBeginEditable name="MainContent" -->
-          <div id="maincontent">
-            <div>
-              <h4><a name="content" id="content"></a>Career Fair Software Form </h4>
+<body id="events">
+
+<!-- NCSTATE Bar -->
+<link rel="stylesheet" type="text/css" href="http://www.ncsu.edu/brand/utility-bar/iframe/css/utility_bar_iframe.css" media="screen" />
+<iframe name="ncsu_branding_bar" id="ncsu_branding_bar" frameborder="0" src="http://www.ncsu.edu/brand/utility-bar/iframe/index.php?color=red&amp;inurl=ncsu.edu/career&amp;center=yes" scrolling="no"> Your browser does not support inline frames or  
+				is currently configured  not to display inline frames.<br />
+Visit <a href="http://ncsu.edu/">http://www.ncsu.edu</a>. </iframe>
+<!-- End of NCSTATE Bar -->
+
+<div id="doc4" class="yui-t2">
+  <div id="hd" role="navigation">
+    <?php require_once('../includefiles/topMenu.php');?>
+  </div>
+  <div id="bd" role="main">
+    <div id="yui-main">
+      <div class="yui-b">
+        <div class="yui-g"><div id="fadeshow1"></div></div>
+        <div id="secContent" class="yui-g">
+          <h1>Career Fair Software Confirmation</h1>
+          
+<div class="breadCrumbHolder module">
+            <div id="breadCrumb" class="breadCrumb module">
+                <ul>
+                <li class="first"> <a href="/career/events">Home</a> </li>
+                <li  class="last">C3M Software Registration</li>
+              </ul>
             </div>
+          </div>
             <p>Please fill out the form below so that we may create your C3M account for the fair. If you have specific questions about the software or setup please contact <a href="mailto:eddie_lovett@ncsu.edu">Eddie Lovett</a>. If you have specific question about pricing and availablity please contact <a href="mailto:carol_schroeder@ncsu.edu">Carol Schroeder</a>. </p>
             <iframe height="768px" width="100%" name="zohoform1"  frameborder="0" scrolling="no" src="http://creator.zoho.com/showForm.do?formLinkId=1&link=true&sharedBy=lovettcreations&submit=Submit Request&nexturl=http://ncsu.edu/career/c3m/confirmation.php&borderColor=FFFFFF&headerColor=FFFFFF&successMsg=Request Added Successfully!&showFormName=false&open=parent&privatelink=7peHQrDwYKE9k07JaX3Zq0bzsrUubK9TSptye4kG0Dnnabm77qntJxJWSPQKwYfPmX73KqueEFBtjdWFB0x3rJx4tY7OdVetGUSd"></iframe>
-          </div>
-        <!-- InstanceEndEditable --></td>
-      </tr>
-      <tr>
-        <td width="100%" colspan="3" align="left" valign="top" bgcolor="#990000">&nbsp;</td>
-      </tr>
-    </table></td>
-  </tr>
-  <!--  <tr>
-    <td colspan="2">safdafdas</td>
-  </tr>-->
-  <tr>
-    <td width="46%" valign="top"><div class="uccfooterinfo">University Career Center <br />
-      2100 Pullen Hall &middot; Box 7303 <br />
-      Raleigh, NC 27695<br />
-      919.515.2396</div></td>
-    <td width="54%" valign="top"><div align="right">
-      <div class="uccfooterinfo">hours: M-F 8:00-5:00<br />
-        drop in hours: M-F 11:00-2:00<br />
-        career_center@ncsu.edu<br />
-        919.515.4210 (fax) </div>
-    </div></td>
-  </tr>
-</table>
-<map name="topheader" id="topheader">
-  <area shape="rect" coords="617,2,757,23" href="http://ncsu.edu/" alt="NCSU Home" />
-</map>
-<div class="skipcontent">
-<a name="site" id="site"></a><a href="/career/students/explore/index.php">Students - Explore</a>
-<a href="/career/students/interviewing/index.php">Students - Interviewing</a>
-<a href="/career/students/jobsinternships/index.php">Students - Jobs and Internship</a>
-<a href="/career/students/quicklinks/staff.php">Students - Meet The Staff</a>
-<a href="/career/students/resumesletters/index.php">Students - Resume</a>
-<a href="/career/employers/Interviewing/policies.php">Employers - Interviewing</a>
-<a href="/career/employers/Marketing/index.php">Employers - Marketing</a>
-<a href="/career/employers/Posting/index.php">Employers - Posting</a>
-<a href="/career/employers/resumes/index.php">Employers - Resumes</a>
-<a href="http://www.alumni.ncsu.edu/">Alumni</a>
-<a href="/career/parents/index.php">Parents</a></div>
-
-<map name="Map" id="Map"><area shape="rect" coords="615,2,759,23" href="http://ncsu.edu" alt="NC State Home Page" />
-</map>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-6631273-1");
-pageTracker._trackPageview();
-</script></body>
-<!-- InstanceEnd --></html>
+        </div>
+      </div>
+    </div>
+    <div class="yui-b">
+      <div id="navTop" class="sideTop">
+        <?php require_once('../includefiles/nav/eventsNav.php');?>
+      </div>
+      <?php require_once('../includefiles/epacklogin.php'); ?>
+      <div id="navMedia" class="sMedia">
+        <?php require_once('../includefiles/socialmedia.php');?>
+      </div>
+    </div>
+  </div>
+  <!-- End of bd -->
+  <div id="ft" role="contentinfo">
+    <?php require_once('../includefiles/footer.php');?>
+  </div>
+</div>
+</body>
+</html>
